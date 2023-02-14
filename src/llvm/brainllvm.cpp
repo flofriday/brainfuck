f3f127a32decda2aee312f0acd44dd6566c8bfcf
flofriday <flohacksfriday@gmail.com>
2023-02-14 16:16:45 +0100

    Start implementing the llvm codegen of the function
    This already feels very exciting, yes at the moment it is only
    an empty function but soon we will have some caculations and
    in the end we will be able to utalize the whole llvm framework. ^^

--- src/llvm/brainllvm.cpp
+++ src/llvm/brainllvm.cpp
@@ -9,6 +9,18 @@
 
 #include "libbytecode.hpp"
 
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
 int main(int argc, char const* argv[])
 {
     // Read input file
@@ -29,11 +41,30 @@ int main(int argc, char const* argv[])
     }
 
     // Compile to llvm IR
+    auto TheContext = std::make_unique<llvm::LLVMContext>();
+    auto TheModule = std::make_unique<llvm::Module>("brainllvm jit", *TheContext);
+    auto Builder = std::make_unique<llvm::IRBuilder<>>(*TheContext);
+
+    // Build a basic function entry into which the whole brainfuck code gets
+    // compiled
+    llvm::FunctionType* FT = llvm::FunctionType::get(llvm::Type::getVoidTy(*TheContext), false);
+    std::string Name = "bf_main";
+    llvm::Function* TheFunction = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, Name, TheModule.get());
+
+    llvm::BasicBlock* BB = llvm::BasicBlock::Create(*TheContext, "entry", TheFunction);
+    Builder->SetInsertPoint(BB);
+    Builder->CreateRet(nullptr);
+    llvm::verifyFunction(*TheFunction);
+    // auto v = llvm::ConstantInt(llvm::IntegerType::getInt64Ty(*TheContext), llvm::APInt((uint64_t)0));
+    TheFunction->print(llvm::errs());
+    return 1;
+
     for (uint64_t i = 0; i < opcodes.size(); i++) {
         // std::cout << instructionPointer << " -> " << dataPointer << std::endl;
         switch (opcodes.at(i)) {
         case OP_MOVE: {
             int8_t argument = readByteArgument(opcodes, i);
+            // Builder->CreateAdd();
             break;
         }
 
